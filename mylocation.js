351e4892addf89d1d17c372ba51ddd2af5ee935b
dvinodeluru <dvinod.eluru@gmail.com>
2017-08-18 17:57:57 +0530

    search list country name

--- mylocation.js
+++ mylocation.js
@@ -141,7 +141,7 @@ var infowindow;
 
         //AddtoGeoInfo(i, storename, country, openDate, sqrMetr, product, roomsetng, relhome, cashlanes, restsets, cowrk, prkspace);
 
-        html += '<li id="' + i + '" class = "loc-list" >' + storename + '</li>';
+        html += '<li id="' + i + '" class = "loc-list" >' + country + '</li>';
         i++;
         
         
@@ -153,7 +153,7 @@ var infowindow;
 
             $('.locDiv').animate({ marginLeft: '-300px' });
             //Title in information panel
-            $('.infoDiv p.title').html('<div class=""> ' + country+'<span class="new">New</span></div>');
+            //$('.infoDiv p.title').html('<div class=""> ' + country +'<span class="new">New</span></div>');
             //Inforamtion of each store in information panel
             //$('.infoDiv p.info').html('<ul><li>Opening date:</li><li>' + openDate + '</li><li> Square metres:</li><li>' + sqrMetr + ' </li><li> Products:</li><li>' + product + '</li><li>Room settings:</li><li>' + roomsetng + ' </li><li>Real-life homes:</li><li>' + relhome + '</li><li> Cash lanes:</li><li>' + cashlanes + '</li><li> Restaurant seats: </li><li>' + restsets + '</li><li>Co-workers:</li><li> ' + cowrk + '</li><li>Parking space:</li><li> ' + prkspace + '</li></ul>');
        
