fb2e5aba23656069fbf1887a97a4c79de29e908d
dvinodeluru <dvinod.eluru@gmail.com>
2017-09-03 14:47:38 +0530

    script changes

--- mylocation.js
+++ mylocation.js
@@ -6,8 +6,11 @@
 
 
 var googleMap = document.getElementById("googleMap");
-var myCenter = new google.maps.LatLng(17.4994889,78.3872775);//map Longitude and Lattitude    
-var markers = [], gmarkers = [], i = 0, html, mylocation, country, content, destination, mylocationL, countryL;
+var myCenter = new google.maps.LatLng(17.4994889, 78.3872775); //map Longitude and Lattitude    
+var markers = [],
+    gmarkers = [],
+    i = 0,
+    html, mylocation, country, content, destination, mylocationL, countryL;
 var locInfo = new Array();
 var infowindow;
 (function initialize() {
@@ -40,7 +43,7 @@ var infowindow;
     html = '<ul>';
     //load xml data in list
     $.get("mylocation.xml", function (data) {
-       
+
         mylocationL = $(data).find('mylocation').size();
 
         countryL = $(data).find('country').size();
@@ -48,40 +51,40 @@ var infowindow;
 
             var eachMarker = $(this);
 
-            country = eachMarker.find("country").text();            
-            sqrMetr = eachMarker.find("content").text();
-            product = eachMarker.find("destination").text();
+            country = eachMarker.find("country").text();
+            content = eachMarker.find("content").text();
+            destination = eachMarker.find("destination").text();
 
             var markerCoords = new google.maps.LatLng(
                 parseFloat(eachMarker.find("latitude").text()),
                 parseFloat(eachMarker.find("longitude").text())
             );
 
-            var infoWin = "<div class='info-blob'>" + country + "</div>";           
+            var infoWin = "<div class='info-blob'>" + country + "</div>";
 
             var marker = addMarker(infoWin, markerCoords, mylocation, country, content, destination);
-           // markers.push(marker);
+            // markers.push(marker);
 
         });
         html += '</ul>';
         //add li's to info div
         $('#locationList').html($(html));
 
-       /* var openDt = parseInt(openDate[0], 10),
-                openMo = parseInt(openDate[1], 10) + 3,
-                openYr = parseInt(openDate[2], 10);
+        /* var openDt = parseInt(openDate[0], 10),
+                 openMo = parseInt(openDate[1], 10) + 3,
+                 openYr = parseInt(openDate[2], 10);
 
-        var now = new Date(),
-          nowYear = now.getFullYear(),
-          nowMo = now.getMonth() + 1, // for getMonth(), January is 0
-          nowDay = now.getDate();
+         var now = new Date(),
+           nowYear = now.getFullYear(),
+           nowMo = now.getMonth() + 1, // for getMonth(), January is 0
+           nowDay = now.getDate();
 
-        //alert(openMo+', '+nowMo);
-        if (nowMo < openMo) {
-            $('.new').toggle('hide');
-            marker.setIcon('https://www.google.com/mapfiles/marker_green.png');
-        }*/
-        
+         //alert(openMo+', '+nowMo);
+         if (nowMo < openMo) {
+             $('.new').toggle('hide');
+             marker.setIcon('https://www.google.com/mapfiles/marker_green.png');
+         }*/
+
         //Header context
         $('.hdrInfo').html('<span>' + countryL + ' countries</span>');
 
@@ -96,12 +99,12 @@ var infowindow;
         //update the sort order
         $('#locationList ul').empty().append(listitems);
 
-       // alert($('#storeList ul > li').length);
+        // alert($('#storeList ul > li').length);
 
         //click on list item to hilighligt the marker with information
         $('#locationList ul > li').each(function () {
             var $li_id = parseInt($(this).attr("id"));
-           // alert($li_id);
+            // alert($li_id);
             $(this).on('click', function () {
                 google.maps.event.trigger(markers[$li_id], 'click');
                 /*for (var z = 0; z < storeInfo.length; z++) {
@@ -111,7 +114,9 @@ var infowindow;
                         break;
                     }
                 }*/
-                $('.locDiv').animate({ marginLeft: '-300px' });
+                $('.locDiv').animate({
+                    marginLeft: '-300px'
+                });
 
                 //Title in information panel
                 //$('.infoDiv p.title').html(country);
@@ -123,12 +128,12 @@ var infowindow;
 
     });
 
-    
+
 
 
     // Create a marker for each XML entry
-    function addMarker(infoWin, markerCoords, storename, country, openDate, sqrMetr, product, roomsetng, relhome, cashlanes, restsets, cowrk, prkspace) {
-        
+    function addMarker(infoWin, markerCoords, mylocation, country, content, destination) {
+
         // Place the new marker
         var marker = new google.maps.Marker({
             map: map,
@@ -136,27 +141,31 @@ var infowindow;
             position: markerCoords
         }); // end place the new marker
 
-        (product === 'true') ? marker.setIcon('mark-on.png') : marker.setIcon('mark-off.png');
+        (destination === 'true') ? marker.setIcon('mark-on.png'): marker.setIcon('mark-off.png');
         gmarkers[i] = marker;
 
         //AddtoGeoInfo(i, storename, country, openDate, sqrMetr, product, roomsetng, relhome, cashlanes, restsets, cowrk, prkspace);
 
         html += '<li id="' + i + '" class = "loc-list" >' + country + '</li>';
         i++;
-        
-        
+
+
         //// Add event listener. On marker click, close all open infoWindows open current infoWindow.   
         google.maps.event.addListener(marker, "click", function () {
             if (infowindow) infowindow.close();
-            infowindow = new google.maps.InfoWindow({ content: infoWin });
+            infowindow = new google.maps.InfoWindow({
+                content: infoWin
+            });
             infowindow.open(map, marker);
 
-            $('.locDiv').animate({ marginLeft: '-300px' });
+            $('.locDiv').animate({
+                marginLeft: '-300px'
+            });
             //Title in information panel
-            $('.infoDiv p.title').html('<div class=""> ' + country +'<span class="new">New</span></div>');
+            $('.infoDiv p.title').html('<div class=""> ' + country + ' <span class="new">New</span></div>');
             //Inforamtion of each store in information panel
-            //$('.infoDiv p.info').html('<ul><li>Opening date:</li><li>' + openDate + '</li><li> Square metres:</li><li>' + sqrMetr + ' </li><li> Products:</li><li>' + product + '</li><li>Room settings:</li><li>' + roomsetng + ' </li><li>Real-life homes:</li><li>' + relhome + '</li><li> Cash lanes:</li><li>' + cashlanes + '</li><li> Restaurant seats: </li><li>' + restsets + '</li><li>Co-workers:</li><li> ' + cowrk + '</li><li>Parking space:</li><li> ' + prkspace + '</li></ul>');
-       
+            $('.infoDiv p.info').html('<ul><li>' + content + '</li></ul>');
+
         }); // end add event listener
 
         /* google.maps.event.addListener(marker, 'mouseover', function () {
@@ -171,7 +180,7 @@ var infowindow;
              infowindow.close(map, marker);
          });*/
 
-     
+
         // Display marker
         markers.push(marker);
 
@@ -194,17 +203,15 @@ var infowindow;
         } else {
             $('#locationList ul > li').each(function () {
                 var text = $(this).text().toLowerCase();
-                (text.indexOf(valThis) >= 0) ? $(this).show() : $(this).hide();
+                (text.indexOf(valThis) >= 0) ? $(this).show(): $(this).hide();
             });
         };
     });
 
     $('p.alink').click(function () {
-        $('.locDiv').animate({ marginLeft: "0" });
+        $('.locDiv').animate({
+            marginLeft: "0"
+        });
         infowindow.close();
     });
 })();
-
-
-
-
